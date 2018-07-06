2a23f59e71458dd3bdb13e9775ddbc13fab6c06b
Tolala9 <anatolii.lukavenko@gmail.com>
2018-07-07 00:19:55 +0300

    Creating start environment: made style task

---
+++ gulp/tasks/watch.js
@@ -0,0 +1,37 @@
+var gulp       = require('gulp'),
+watch          = require('gulp-watch'),
+browserSync    = require('browser-sync').create();
+
+gulp.task('watch', function() {
+
+  browserSync.init({
+      server: {
+        baseDir: "app"
+      },
+      notify: false
+  });
+
+  watch('./app/index.html', function() {
+    browserSync.reload();
+  });
+
+  watch('./app/assets/styles/**/*.sass', function() {
+    gulp.start('waitForStyles');
+  });
+
+  // watch('./app/assets/scripts/**/*.js', function() {
+  //   gulp.start('scriptsRefresh');
+  // });
+
+});
+
+gulp.task('waitForStyles', ['styles'], function(){
+  return gulp.src('./app/temp/styles/style.css')
+  .pipe(browserSync.stream());
+});
+
+// gulp.task('waitForScripts', ['scripts'], function(){
+//   browserSync.reload();
+// });
+
+gulp.task('default', ['watch']);
