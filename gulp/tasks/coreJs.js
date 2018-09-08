cbf122abdd23192756cf462ea054b741b20aa6c3
Anatolii <anatolii.lukavenko@gmail.com>
2018-09-08 10:59:26 +0300

    JS: set up material-parallax for section about

--- gulp/tasks/coreJs.js
+++ gulp/tasks/coreJs.js
@@ -5,7 +5,8 @@ uglify = require('gulp-uglify');
 gulp.task('coreJs', function() {
 	return gulp.src([
 		'./app/assets/libs/Jquery_2.2.4/jquery_2.24.min.js',
-		'./app/assets/libs/OwlCarousel/owl.carousel.js'
+		'./app/assets/libs/OwlCarousel/owl.carousel.js',
+		'./app/assets/libs/Parallax/materianizeParallax.js'
 		])
 	.pipe(concat('core.min.js'))
 	.pipe(uglify()) // minimize
